d1da0e9e9223cb46db14b070e0e68ec8e96cdfbb
Insane-96 <insane96mcp@gmail.com>
2019-02-10 10:06:39 +0100

    Added buggy info to the property

--- common/net/insane96mcp/iguanatweaks/lib/Properties.java
+++ common/net/insane96mcp/iguanatweaks/lib/Properties.java
@@ -83,7 +83,7 @@ public class Properties {
 			@RangeDouble(min = 0, max = Float.MAX_VALUE)
 			public float exhaustionMultiplier = 1.0f;
 			@Name("No Item No Knockback")
-			@Comment("If the player attacks an entity without a tool / weapon, the attacked mob will take almost no damage (1/20 of a heart) and no knockback. The no knockback applies even if the player attack speed cooldown is below 75% with a tool / weapon, but in this case the damage is applied normally")
+			@Comment("If the player attacks an entity without a tool / weapon, the attacked mob will take almost no damage (1/20 of a heart) and no knockback. The no knockback applies even if the player attack speed cooldown is below 75% with a tool / weapon, but in this case the damage is applied normally.\nThis feature is disabled by default since is a little bit buggy, like no experience is dropped from the mob if it gets attacked only by No Knockbacks attacks.")
 			public boolean noItemNoKnockback = false;
 		}
 
