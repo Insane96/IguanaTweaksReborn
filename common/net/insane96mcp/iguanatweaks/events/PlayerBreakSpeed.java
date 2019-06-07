ddeff5e46e5440ce9148b57ba252b123a9c45101
Insane-96 <insane96mcp@gmail.com>
2019-06-07 10:01:57 +0200

    1.4.15 - Breaking event has been set as last to be executed so other mods can mess with breaking speed before applying the global hardness

--- common/net/insane96mcp/iguanatweaks/events/PlayerBreakSpeed.java
+++ common/net/insane96mcp/iguanatweaks/events/PlayerBreakSpeed.java
@@ -4,12 +4,13 @@ import net.insane96mcp.iguanatweaks.IguanaTweaks;
 import net.insane96mcp.iguanatweaks.modules.ModuleHardness;
 import net.minecraftforge.event.entity.player.PlayerEvent.BreakSpeed;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber(modid = IguanaTweaks.MOD_ID)
 public class PlayerBreakSpeed {
 
-	@SubscribeEvent
+	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public static void EventBreakSpeed(BreakSpeed event) {
 		ModuleHardness.ProcessHardness(event);
 	}
