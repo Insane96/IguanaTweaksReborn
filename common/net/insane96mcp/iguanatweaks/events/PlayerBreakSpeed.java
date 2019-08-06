fa8cae4ff750b21c810e23b2456e66881d31eaa4
Insane-96 <insane96mcp@gmail.com>
2019-08-06 21:40:57 +0200

    1.4.20 - Hopefully fixed #89

--- common/net/insane96mcp/iguanatweaks/events/PlayerBreakSpeed.java
+++ common/net/insane96mcp/iguanatweaks/events/PlayerBreakSpeed.java
@@ -4,10 +4,12 @@ import net.insane96mcp.iguanatweaks.IguanaTweaks;
 import net.insane96mcp.iguanatweaks.modules.ModuleHardness;
 import net.minecraftforge.event.entity.player.PlayerEvent.BreakSpeed;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber(modid = IguanaTweaks.MOD_ID)
 public class PlayerBreakSpeed {
-
+	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public static void EventBreakSpeed(BreakSpeed event) {
 		ModuleHardness.ProcessHardness(event);
 	}
