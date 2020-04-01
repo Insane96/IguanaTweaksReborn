23c4c7646bfb3ba0b1e09ba10d254b1a0ae0a046
Insane96 <insane96mcp@gmail.com>
2020-04-01 10:40:10 +0200

    Moved isInTagOrBlock and added isInTagOrItem into Utils Also added a null check in global hardness for dimension

--- src/main/java/insane96mcp/iguanatweaksreborn/modules/HardnessModule.java
+++ src/main/java/insane96mcp/iguanatweaksreborn/modules/HardnessModule.java
@@ -4,7 +4,6 @@ import insane96mcp.iguanatweaksreborn.setup.ModConfig;
 import insane96mcp.iguanatweaksreborn.utils.Utils;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -20,33 +19,26 @@ public class HardnessModule {
     public static void ProcessGlobalHardness(PlayerEvent.BreakSpeed event) {
         if (!ModConfig.Modules.hardness)
             return;
-
         if (ModConfig.Hardness.multiplier == 1.0d && ModConfig.Hardness.dimensionMultipliers.size() == 0)
             return;
-
         World world = event.getPlayer().world;
         Dimension dimension = world.getDimension();
         ResourceLocation dimensionId = dimension.getType().getRegistryName();
-
+        if (dimensionId == null)
+            dimensionId = Utils.AnyRL;
         BlockState blockState = world.getBlockState(event.getPos());
-
         Block block = blockState.getBlock();
-        ResourceLocation blockId = block.getRegistryName();
-
         for (ModConfig.Hardness.BlockHardness blockHardness : ModConfig.Hardness.customHardness)
-            if (isInTagOrBlock(blockHardness, block, blockId, dimensionId))
+            if (Utils.isInTagOrBlock(blockHardness, block, dimensionId))
                 return;
-
         boolean isInWhitelist = false;
-
-        for (ModConfig.Hardness.CommonTagBlock blacklistEntry : ModConfig.Hardness.blacklist) {
+        for (ModConfig.IdTagMatcher blacklistEntry : ModConfig.Hardness.blacklist) {
             if (!ModConfig.Hardness.blacklistAsWhitelist) {
-                if (isInTagOrBlock(blacklistEntry, block, blockId, dimensionId))
+                if (Utils.isInTagOrBlock(blacklistEntry, block, dimensionId))
                     return;
             }
             else {
-                isInWhitelist = false;
-                if (isInTagOrBlock(blacklistEntry, block, blockId, dimensionId)) {
+                if (Utils.isInTagOrBlock(blacklistEntry, block, dimensionId)) {
                     isInWhitelist = true;
                     break;
                 }
@@ -83,12 +75,11 @@ public class HardnessModule {
         BlockState blockState = world.getBlockState(pos);
 
         Block block = blockState.getBlock();
-        ResourceLocation blockId = block.getRegistryName();
 
         double hardness = -1;
 
         for (ModConfig.Hardness.BlockHardness blockHardness : ModConfig.Hardness.customHardness) {
-            if (isInTagOrBlock(blockHardness, block, blockId, dimensionId)) {
+            if (Utils.isInTagOrBlock(blockHardness, block, dimensionId)) {
                 hardness = blockHardness.hardness;
                 break;
             }
@@ -104,20 +95,4 @@ public class HardnessModule {
         return state.getBlockHardness(world, pos) / newHardness;
     }
 
-    private static boolean isInTagOrBlock(ModConfig.Hardness.CommonTagBlock commonTagBlock, Block block, ResourceLocation blockId, ResourceLocation dimensionId) {
-        if (commonTagBlock.tag != null) {
-            if (!BlockTags.getCollection().getRegisteredTags().contains(commonTagBlock.tag))
-                return false;
-
-            if (BlockTags.getCollection().get(commonTagBlock.tag).contains(block))
-                if ((!commonTagBlock.dimension.equals(Utils.AnyRL) && commonTagBlock.dimension.equals(dimensionId)) || commonTagBlock.dimension.equals(Utils.AnyRL))
-                    return true;
-        }
-        else {
-            if (blockId.equals(commonTagBlock.block))
-                if ((!commonTagBlock.dimension.equals(Utils.AnyRL) && commonTagBlock.dimension.equals(dimensionId)) || commonTagBlock.dimension.equals(Utils.AnyRL))
-                    return true;
-        }
-        return false;
-    }
 }
