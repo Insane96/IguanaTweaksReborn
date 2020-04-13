51a53249c80ef70d7c9b0375db5d3568bd3cb9f4
Insane96 <insane96mcp@gmail.com>
2020-04-13 08:26:09 +0200

    Fixed #105 Was using wrong event

--- src/main/java/insane96mcp/iguanatweaksreborn/modules/SleepRespawnModule.java
+++ src/main/java/insane96mcp/iguanatweaksreborn/modules/SleepRespawnModule.java
@@ -1,32 +1,28 @@
 package insane96mcp.iguanatweaksreborn.modules;
 
 import insane96mcp.iguanatweaksreborn.setup.ModConfig;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
-import net.minecraftforge.event.world.SleepFinishedTimeEvent;
+import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.stream.Collectors;
-
 public class SleepRespawnModule {
-	public static void wakeUpHungerAndEffects(SleepFinishedTimeEvent event) {
+	public static void wakeUpHungerAndEffects(PlayerWakeUpEvent event) {
 		if (!ModConfig.Modules.sleepRespawn)
 			return;
 		if (ModConfig.SleepRespawn.hungerDepletedOnWakeUp == 0 && ModConfig.SleepRespawn.effectsOnWakeUp.isEmpty())
 			return;
-		event.getWorld().getPlayers().stream().filter(LivingEntity::isSleeping).collect(Collectors.toList()).forEach((player) -> {
-			player.getFoodStats().addStats(-ModConfig.SleepRespawn.hungerDepletedOnWakeUp, 1.0f);
-			//For some reasons saturation can go below 0 so I get it back up to 0
-			if (player.getFoodStats().getSaturationLevel() < 0.0f)
-				player.getFoodStats().addStats(1, -player.getFoodStats().getSaturationLevel() / 2f);
-			for (ModConfig.SleepRespawn.EffectOnWakeUp effectOnWakeUp : ModConfig.SleepRespawn.effectsOnWakeUp) {
-				EffectInstance effectInstance = new EffectInstance(ForgeRegistries.POTIONS.getValue(effectOnWakeUp.potionId), effectOnWakeUp.duration, effectOnWakeUp.amplifier, true, true);
-				player.addPotionEffect(effectInstance);
-			}
-		});
+		PlayerEntity player = event.getPlayer();
+		player.getFoodStats().addStats(-ModConfig.SleepRespawn.hungerDepletedOnWakeUp, 1.0f);
+		//For some reasons saturation can go below 0 so I get it back up to 0
+		if (player.getFoodStats().getSaturationLevel() < 0.0f)
+			player.getFoodStats().addStats(1, -player.getFoodStats().getSaturationLevel() / 2f);
+		for (ModConfig.SleepRespawn.EffectOnWakeUp effectOnWakeUp : ModConfig.SleepRespawn.effectsOnWakeUp) {
+			EffectInstance effectInstance = new EffectInstance(ForgeRegistries.POTIONS.getValue(effectOnWakeUp.potionId), effectOnWakeUp.duration, effectOnWakeUp.amplifier, true, true);
+			player.addPotionEffect(effectInstance);
+		}
 	}
 
 	public static void tooHungryToSleep(PlayerSleepInBedEvent event) {
