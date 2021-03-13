d3807e098870aa332e9512bc221ff1100799e39d
Insane96 <insane96mcp@gmail.com>
2021-03-13 13:32:08 +0100

    Added Tool Nerf module

---
+++ src/main/java/insane96mcp/iguanatweaksreborn/modules/misc/classutils/ToolDurability.java
@@ -0,0 +1,43 @@
+package insane96mcp.iguanatweaksreborn.modules.misc.classutils;
+
+
+import insane96mcp.iguanatweaksreborn.common.classutils.IdTagMatcher;
+import insane96mcp.iguanatweaksreborn.utils.LogHelper;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.apache.commons.lang3.math.NumberUtils;
+
+import javax.annotation.Nullable;
+
+public class ToolDurability extends IdTagMatcher {
+	public int durability;
+
+	public ToolDurability(@Nullable ResourceLocation item, int durability) {
+		super(item, null);
+		this.durability = durability;
+	}
+
+	@Nullable
+	public static ToolDurability parseLine(String line) {
+		String[] split = line.split(",");
+		if (split.length != 2) {
+			LogHelper.Warn("Invalid line \"%s\" for Custom Tool Durability", line);
+			return null;
+		}
+		ResourceLocation item = ResourceLocation.tryCreate(split[0]);
+		if (item == null) {
+			LogHelper.Warn("%s item for Tool Durability is not valid", split[0]);
+			return null;
+		}
+		if (!ForgeRegistries.ITEMS.containsKey(item)) {
+			LogHelper.Warn(String.format("%s item for Tool Durability seems to not exist", split[0]));
+			return null;
+		}
+		if (!NumberUtils.isParsable(split[1])) {
+			LogHelper.Warn(String.format("Invalid durability \"%s\" for Tool Durability", line));
+			return null;
+		}
+		int durability = Integer.parseInt(split[1]);
+		return new ToolDurability(item, durability);
+	}
+}
---
+++ src/main/java/insane96mcp/iguanatweaksreborn/modules/misc/feature/ToolNerfFeature.java
@@ -0,0 +1,64 @@
+package insane96mcp.iguanatweaksreborn.modules.misc.feature;
+
+import insane96mcp.iguanatweaksreborn.base.ITFeature;
+import insane96mcp.iguanatweaksreborn.base.ITModule;
+import insane96mcp.iguanatweaksreborn.base.Label;
+import insane96mcp.iguanatweaksreborn.modules.misc.classutils.ToolDurability;
+import insane96mcp.iguanatweaksreborn.setup.Config;
+import net.minecraft.item.TieredItem;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Label(name = "Tool Nerf", description = "Less durable tools")
+public class ToolNerfFeature extends ITFeature {
+    private final ForgeConfigSpec.ConfigValue<List<? extends String>> toolsDurabilityConfig;
+
+    private final List<String> toolsDurabilityDefault = Arrays.asList(
+            "minecraft:wooden_sword,1", "minecraft:wooden_pickaxe,1", "minecraft:wooden_shovel,1", "minecraft:wooden_hoe,1", "minecraft:wooden_axe,10",
+            "minecraft:stone_sword,6", "minecraft:stone_pickaxe,6", "minecraft:stone_shovel,50", "minecraft:stone_hoe,6", "minecraft:stone_axe,50"
+    );
+
+    public ArrayList<ToolDurability> toolsDurability;
+
+    public ToolNerfFeature(ITModule module) {
+        super(module);
+        Config.builder.comment(this.getDescription()).push(this.getName());
+        toolsDurabilityConfig = Config.builder
+                .comment("A list of items which should have their durability changed.\n" +
+                        "Format is 'modid:itemid,durability'")
+                .defineList("Debuffs", toolsDurabilityDefault, o -> o instanceof String);
+        Config.builder.pop();
+    }
+
+    @Override
+    public void loadConfig() {
+        super.loadConfig();
+        toolsDurability = parseDurabilities(toolsDurabilityConfig.get());
+        for (ToolDurability toolDurability : vanillaDurabilities) {
+            TieredItem item = (TieredItem) ForgeRegistries.ITEMS.getValue(toolDurability.id);
+            item.maxDamage = toolDurability.durability;
+        }
+        vanillaDurabilities.clear();
+        for (ToolDurability toolDurability : toolsDurability) {
+            TieredItem item = (TieredItem) ForgeRegistries.ITEMS.getValue(toolDurability.id);
+            vanillaDurabilities.add(new ToolDurability(toolDurability.id, item.maxDamage));
+            item.maxDamage = toolDurability.durability;
+        }
+    }
+
+    private final ArrayList<ToolDurability> vanillaDurabilities = new ArrayList<>();
+
+    private static ArrayList<ToolDurability> parseDurabilities(List<? extends String> list) {
+        ArrayList<ToolDurability> toolDurabilities = new ArrayList<>();
+        for (String line : list) {
+            ToolDurability toolDurability = ToolDurability.parseLine(line);
+            if (toolDurability != null)
+                toolDurabilities.add(toolDurability);
+        }
+        return toolDurabilities;
+    }
+}
