93932abd98aa7cf1fe75f2072b4e6fb92d06e4b9
Insane96 <insane96mcp@gmail.com>
2021-04-03 12:42:21 +0200

    Refined No Pillaring Feature: If the block is not solid (e.g. Torch) can now be placed anyway Blocks placed more than 1.1 (scales with Jump Boost) blocks away from the player will now be placed anyway Blocks that aren't below the player will be placed anyway (player Y <= block placed Y)

--- src/main/java/insane96mcp/iguanatweaksreborn/modules/movement/feature/NoPillaringFeature.java
+++ src/main/java/insane96mcp/iguanatweaksreborn/modules/movement/feature/NoPillaringFeature.java
@@ -4,8 +4,12 @@ import insane96mcp.iguanatweaksreborn.setup.Config;
 import insane96mcp.insanelib.base.Feature;
 import insane96mcp.insanelib.base.Label;
 import insane96mcp.insanelib.base.Module;
+import net.minecraft.block.Block;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -26,14 +30,25 @@ public class NoPillaringFeature extends Feature {
 
 	@SubscribeEvent
     public void playerTick(PlayerInteractEvent.RightClickBlock event) {
-        if (!this.isEnabled())
-            return;
-        PlayerEntity playerEntity = event.getPlayer();
-        if (playerEntity.isCreative() || playerEntity.isInWater() || playerEntity.isOnLadder())
-            return;
-        if (playerEntity.getPitch(1.0f) > 40f && !playerEntity.isOnGround() && event.getItemStack().getItem() instanceof BlockItem) {
-            event.setCanceled(true);
-            event.setResult(Event.Result.DENY);
-        }
-    }
+		if (!this.isEnabled())
+			return;
+		PlayerEntity playerEntity = event.getPlayer();
+		if (playerEntity.isCreative() || playerEntity.isInWater() || playerEntity.isOnLadder())
+			return;
+		BlockPos placedPos = event.getPos().offset(event.getFace());
+		Vector3d placedBlock = new Vector3d(placedPos.getX() + 0.5d, placedPos.getY() + 0.5d, placedPos.getZ() + 0.5d);
+		double distance = placedBlock.distanceTo(playerEntity.getPositionVec());
+		double allowedDistance = 1.1d;
+		if (playerEntity.isPotionActive(Effects.JUMP_BOOST))
+			allowedDistance *= 1 + ((playerEntity.getActivePotionEffect(Effects.JUMP_BOOST).getAmplifier() + 1) * 0.5);
+		boolean isSolidBlock = true;
+		if (event.getItemStack().getItem() instanceof BlockItem) {
+			Block block = ((BlockItem) event.getItemStack().getItem()).getBlock();
+			isSolidBlock = block.getDefaultState().isSolid();
+		}
+		if (playerEntity.getPitch(1.0f) > 40f && !playerEntity.isOnGround() && event.getItemStack().getItem() instanceof BlockItem && distance <= allowedDistance && playerEntity.getPosY() > placedPos.getY() && isSolidBlock) {
+			event.setCanceled(true);
+			event.setResult(Event.Result.DENY);
+		}
+	}
 }
