7960817e5972e80dc853cfe7b2fb05a937787348
Insane96MCP <delvillano.alberto@gmail.com>
2021-07-04 10:23:04 +0200

    Added Wrong Tool feature, disabled by default. Closes #139, #145

--- src/main/java/insane96mcp/iguanatweaksreborn/modules/mining/MiningModule.java
+++ src/main/java/insane96mcp/iguanatweaksreborn/modules/mining/MiningModule.java
@@ -2,6 +2,7 @@ package insane96mcp.iguanatweaksreborn.modules.mining;
 
 import insane96mcp.iguanatweaksreborn.modules.mining.feature.CustomHardnessFeature;
 import insane96mcp.iguanatweaksreborn.modules.mining.feature.GlobalHardnessFeature;
+import insane96mcp.iguanatweaksreborn.modules.mining.feature.WrongToolFeature;
 import insane96mcp.iguanatweaksreborn.setup.Config;
 import insane96mcp.insanelib.base.Label;
 import insane96mcp.insanelib.base.Module;
@@ -11,12 +12,14 @@ public class MiningModule extends Module {
 
 	public GlobalHardnessFeature globalHardness;
 	public CustomHardnessFeature customHardness;
+	public WrongToolFeature wrongTool;
 
 	public MiningModule() {
 		super(Config.builder);
 		pushConfig(Config.builder);
 		globalHardness = new GlobalHardnessFeature(this);
 		customHardness = new CustomHardnessFeature(this);
+		wrongTool = new WrongToolFeature(this);
 		Config.builder.pop();
 	}
 
@@ -25,5 +28,6 @@ public class MiningModule extends Module {
 		super.loadConfig();
 		globalHardness.loadConfig();
 		customHardness.loadConfig();
+		wrongTool.loadConfig();
 	}
 }
