20c28202c7ff1372a0a0b445788e5479ced82f72
Insane96MCP <delvillano.alberto@gmail.com>
2021-09-04 11:06:48 +0200

    Nerfed Bonemeal now makes use of the new BlacklistConfig This adds a blacklist as whitelist config. Fixed error on startup regarding the unaffected blocks tag

--- src/main/java/insane96mcp/iguanatweaksreborn/modules/farming/feature/NerfedBonemealFeature.java
+++ src/main/java/insane96mcp/iguanatweaksreborn/modules/farming/feature/NerfedBonemealFeature.java
@@ -8,6 +8,7 @@ import insane96mcp.iguanatweaksreborn.setup.Config;
 import insane96mcp.insanelib.base.Feature;
 import insane96mcp.insanelib.base.Label;
 import insane96mcp.insanelib.base.Module;
+import insane96mcp.insanelib.config.BlacklistConfig;
 import insane96mcp.insanelib.utils.IdTagMatcher;
 import insane96mcp.insanelib.utils.RandomHelper;
 import net.minecraft.block.BeetrootBlock;
@@ -22,7 +23,6 @@ import net.minecraftforge.event.entity.player.BonemealEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -32,15 +32,17 @@ public class NerfedBonemealFeature extends Feature {
 
 	private final ForgeConfigSpec.ConfigValue<NerfedBonemeal> nerfedBonemealConfig;
 	private final ForgeConfigSpec.ConfigValue<Double> bonemealFailChanceConfig;
-	private final ForgeConfigSpec.ConfigValue<List<? extends String>> itemBlacklistConfig;
-	private final ForgeConfigSpec.ConfigValue<List<? extends String>> blockBlacklsitConfig;
+	private final BlacklistConfig itemBlacklistConfig;
+	private final BlacklistConfig blockBlacklistConfig;
 
-	private static final List<String> blockBlacklsitDefault = Arrays.asList("#iguanatweaksreborn:bonemeal_nerf_unaffected_crops");
+	private static final List<String> blockBlacklistDefault = Arrays.asList("supplementaries:flax");
 
 	public NerfedBonemeal nerfedBonemeal = NerfedBonemeal.NERFED;
 	public double bonemealFailChance = 0d;
 	public List<IdTagMatcher> itemBlacklist;
+	public boolean itemBlacklistAsWhitelist = false;
 	public List<IdTagMatcher> blockBlacklist;
+	public boolean blockBlacklistAsWhitelist = false;
 
 	public NerfedBonemealFeature(Module module) {
 		super(Config.builder, module);
@@ -51,14 +53,12 @@ public class NerfedBonemealFeature extends Feature {
 		bonemealFailChanceConfig = Config.builder
 				.comment("Makes Bone Meal have a chance to fail to grow crops. 0 to disable, 1 to disable bonemeal.")
 				.defineInRange("Bonemeal Fail Chance", bonemealFailChance, 0d, 1d);
-		itemBlacklistConfig = Config.builder
-				.comment("Items or item tags that will ignore the feature. Can be used with any item that inherits the properties of vanilla bonemeal (and it's properly implemented).\n" +
-						"Each entry has an item or tag. The format is modid:item_id or #modid:item_tag.")
-				.defineList("Item Blacklist", new ArrayList<>(), o -> o instanceof String);
-		blockBlacklsitConfig = Config.builder
-				.comment("Blocks or block tags that will not be affected by the bonemeal nerf.\n" +
-						"Each entry has a block or a block tag. The format is modid:block_id or #modid:block_tag.")
-				.defineList("Block Blacklist", new ArrayList<>(), o -> o instanceof String);
+		itemBlacklistConfig = new BlacklistConfig(Config.builder, "Item Blacklist",
+				"Items or item tags that will ignore the feature. Can be used with any item that inherits the properties of vanilla bonemeal (and it's properly implemented).\n" +
+				"Each entry has an item or tag. The format is modid:item_id or #modid:item_tag.", Collections.emptyList(), this.itemBlacklistAsWhitelist);
+		blockBlacklistConfig = new BlacklistConfig(Config.builder, "Block Blacklist",
+				"Blocks or block tags that will not be affected by the bonemeal nerf.\n" +
+				"Each entry has a block or a block tag. The format is modid:block_id or #modid:block_tag.", blockBlacklistDefault, this.blockBlacklistAsWhitelist);
 		Config.builder.pop();
 	}
 
@@ -67,8 +67,10 @@ public class NerfedBonemealFeature extends Feature {
 		super.loadConfig();
 		this.nerfedBonemeal = this.nerfedBonemealConfig.get();
 		this.bonemealFailChance = this.bonemealFailChanceConfig.get();
-		this.itemBlacklist = IdTagMatcher.parseStringList(this.itemBlacklistConfig.get());
-		this.blockBlacklist = IdTagMatcher.parseStringList(this.blockBlacklsitConfig.get());
+		this.itemBlacklist = IdTagMatcher.parseStringList(this.itemBlacklistConfig.listConfig.get());
+		this.itemBlacklistAsWhitelist = this.itemBlacklistConfig.listAsWhitelistConfig.get();
+		this.blockBlacklist = IdTagMatcher.parseStringList(this.blockBlacklistConfig.listConfig.get());
+		this.blockBlacklistAsWhitelist = this.blockBlacklistConfig.listAsWhitelistConfig.get();
 	}
 
 	/**
@@ -96,14 +98,36 @@ public class NerfedBonemealFeature extends Feature {
 	}
 
 	public BonemealResult applyBonemeal(World world, ItemStack stack, BlockState state, BlockPos pos) {
-		for (IdTagMatcher idTagMatcher : itemBlacklist) {
-			if (idTagMatcher.matchesItem(stack.getItem()))
-				return BonemealResult.NONE;
+		//Check for item black/whitelist
+		boolean isInWhitelist = false;
+		boolean isInBlacklist = false;
+		for (IdTagMatcher blacklistEntry : this.itemBlacklist) {
+			if (blacklistEntry.matchesItem(stack.getItem())) {
+				if (!this.itemBlacklistAsWhitelist)
+					isInBlacklist = true;
+				else
+					isInWhitelist = true;
+				break;
+			}
 		}
-		for (IdTagMatcher idTagMatcher : blockBlacklist) {
-			if (idTagMatcher.matchesBlock(state.getBlock()))
-				return BonemealResult.NONE;
+		if (isInBlacklist || (!isInWhitelist && this.itemBlacklistAsWhitelist))
+			return BonemealResult.NONE;
+
+		//Check for block black/whitelist
+		isInWhitelist = false;
+		isInBlacklist = false;
+		for (IdTagMatcher blacklistEntry : this.blockBlacklist) {
+			if (blacklistEntry.matchesBlock(state.getBlock())) {
+				if (!this.itemBlacklistAsWhitelist)
+					isInBlacklist = true;
+				else
+					isInWhitelist = true;
+				break;
+			}
 		}
+		if (isInBlacklist || (!isInWhitelist && this.blockBlacklistAsWhitelist))
+			return BonemealResult.NONE;
+
 		//If farmland is dry and cropsRequireWater is set to ANY_CASE then cancel the event
 		if (FarmingModule.isAffectedByFarmlandState(world, pos) && !FarmingModule.isCropOnWetFarmland(world, pos) && Modules.farming.cropsGrowth.cropsRequireWater.equals(CropsRequireWater.ANY_CASE)) {
 			return BonemealResult.CANCEL;
