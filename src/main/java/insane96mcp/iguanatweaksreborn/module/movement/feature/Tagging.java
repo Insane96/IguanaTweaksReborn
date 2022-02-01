66ae4e92766436ba8044fe4a5aa48d76931f1a17
Insane96MCP <delvillano.alberto@gmail.com>
2022-02-01 07:52:24 +0100

    Tagging has now config options (also increased slowdown duration)

--- src/main/java/insane96mcp/iguanatweaksreborn/module/movement/feature/Tagging.java
+++ src/main/java/insane96mcp/iguanatweaksreborn/module/movement/feature/Tagging.java
@@ -8,19 +8,34 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 @Label(name = "Tagging", description = "Player's slowed down for a brief moment when hit.")
 public class Tagging extends Feature {
 
+	private final ForgeConfigSpec.ConfigValue<Integer> slownessLevelConfig;
+	private final ForgeConfigSpec.ConfigValue<Integer> durationMultiplierConfig;
+
+	public int slownessLevel = 1;
+	public int durationMultiplier = 6;
+
 	public Tagging(Module module) {
 		super(Config.builder, module);
+		slownessLevelConfig = Config.builder
+				.comment("Which level of Slowness is applied to the player (level 0 is Slowness I).")
+				.defineInRange("Slowness level", this.slownessLevel, 0, 10);
+		durationMultiplierConfig = Config.builder
+				.comment("Slowness is applied for damage_taken * this_value ticks.")
+				.defineInRange("Duration multiplier", this.durationMultiplier, 0, 100);
 	}
 
 	@Override
 	public void loadConfig() {
 		super.loadConfig();
+		this.slownessLevel = this.slownessLevelConfig.get();
+		this.durationMultiplier = this.durationMultiplierConfig.get();
 	}
 
 	@SubscribeEvent
@@ -30,6 +45,6 @@ public class Tagging extends Feature {
 		if (!(event.getEntity() instanceof Player playerEntity))
 			return;
 		if (event.getSource().getEntity() instanceof LivingEntity)
-			playerEntity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, (int) (event.getAmount() * 5), 1, false, false, true));
+			playerEntity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, (int) (event.getAmount() * this.durationMultiplier), this.slownessLevel, false, false, true));
 	}
 }
