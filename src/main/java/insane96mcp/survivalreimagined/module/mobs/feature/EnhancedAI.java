9ba56565c7cd541f25eb1ec94ce9f545f6c1483a
Insane96MCP <delvillano.alberto@gmail.com>
2023-03-25 17:02:22 +0100

    Added EnhancedAI

---
+++ src/main/java/insane96mcp/survivalreimagined/module/mobs/feature/EnhancedAI.java
@@ -0,0 +1,37 @@
+package insane96mcp.survivalreimagined.module.mobs.feature;
+
+import insane96mcp.enhancedai.modules.creeper.feature.CreeperSwell;
+import insane96mcp.enhancedai.modules.skeleton.feature.SkeletonFleeTarget;
+import insane96mcp.insanelib.base.Label;
+import insane96mcp.insanelib.base.Module;
+import insane96mcp.insanelib.base.config.Config;
+import insane96mcp.insanelib.base.config.LoadFeature;
+import insane96mcp.survivalreimagined.base.SRFeature;
+import insane96mcp.survivalreimagined.module.Modules;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+
+@Label(name = "EnhancedAI", description = "Changes to EnhancedAI config")
+@LoadFeature(module = Modules.Ids.MOBS)
+public class EnhancedAI extends SRFeature {
+    @Config
+    @Label(name = "Cena Nerf", description = "Makes creeper Cena explosion power the same as normal creeper. Cena will be just a jumpscare lul.")
+    public static Boolean cenaNerf = true;
+
+    public EnhancedAI(Module module, boolean enabledByDefault, boolean canBeDisabled) {
+        super(module, enabledByDefault, canBeDisabled);
+    }
+
+    @Override
+    public void readConfig(ModConfigEvent event) {
+        super.readConfig(event);
+
+        if (cenaNerf) {
+            Module.getFeature(CreeperSwell.class).setConfigOption("Cena.Explosion power", 4d);
+        }
+
+        Module.getFeature(SkeletonFleeTarget.class).setConfigOption("Flee speed Multiplier Near", 1.1d);
+        Module.getFeature(SkeletonFleeTarget.class).setConfigOption("Flee speed Multiplier Far", 1d);
+        //Read the config values
+        Module.getFeature(CreeperSwell.class).readConfig(event);
+    }
+}
--- src/main/java/insane96mcp/survivalreimagined/module/mobs/feature/StatsBuffs.java
+++ src/main/java/insane96mcp/survivalreimagined/module/mobs/feature/StatsBuffs.java
@@ -1,55 +1,20 @@
 package insane96mcp.survivalreimagined.module.mobs.feature;
 
+import insane96mcp.enhancedai.modules.base.feature.Targeting;
 import insane96mcp.insanelib.base.Feature;
 import insane96mcp.insanelib.base.Label;
 import insane96mcp.insanelib.base.Module;
 import insane96mcp.insanelib.base.config.Config;
 import insane96mcp.insanelib.base.config.LoadFeature;
+import insane96mcp.insanelib.base.config.MinMax;
 import insane96mcp.survivalreimagined.module.Modules;
 import insane96mcp.survivalreimagined.setup.IntegratedDataPacks;
 import net.minecraft.server.packs.PackType;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
 
 @Label(name = "Stats Buffs", description = "Increase monsters health, movement speed, etc. This feature uses Mobs Properties Randomness.")
 @LoadFeature(module = Modules.Ids.MOBS)
 public class StatsBuffs extends Feature {
-
-    /*public static final String STAT_BUFFS_APPLIED = SurvivalReimagined.RESOURCE_PREFIX + "stat_buffs_applied";
-    public static final UUID MOVEMENT_SPEED_UUID = UUID.fromString("f62c1940-39a9-4a4a-8421-2278e259e5f6");
-    public static final UUID HEALTH_UUID = UUID.fromString("ad58b57d-1956-416d-804a-5180a01d0bad");
-    public static final UUID SWIM_SPEED_UUID = UUID.fromString("ed9f51b5-e297-498e-a909-31644f2a326d");
-
-    @Config(min = 0)
-    @Label(name = "Movement Speed.Easy", description = "Bonus percentage movement speed given to mobs on easy difficulty.")
-    public static Double movementSpeedEasy = 0.1d;
-    @Config(min = 0)
-    @Label(name = "Movement Speed.Normal", description = "Bonus percentage movement speed given to mobs on normal difficulty.")
-    public static Double movementSpeedNormal = 0.2d;
-    @Config(min = 0)
-    @Label(name = "Movement Speed.Hard", description = "Bonus percentage movement speed given to mobs on hard difficulty.")
-    public static Double movementSpeedHard = 0.3d;
-
-    @Config(min = 0)
-    @Label(name = "Health.Easy", description = "Bonus percentage health given to mobs on easy difficulty.")
-    public static Double healthEasy = 0.2d;
-    @Config(min = 0)
-    @Label(name = "Health.Normal", description = "Bonus percentage health given to mobs on normal difficulty.")
-    public static Double healthNormal = 0.5d;
-    @Config(min = 0)
-    @Label(name = "Health.Hard", description = "Bonus percentage health given to mobs on hard difficulty.")
-    public static Double healthHard = 0.75d;
-
-    @Config(min = 0)
-    @Label(name = "Follow Range.Easy", description = "Override for mobs' follow range on easy difficulty. The override is not applied if the mob's follow range is already higher than this. Set to 0 to not override Follow Range.")
-    public static Integer followRangeEasy = 24;
-    @Config(min = 0)
-    @Label(name = "Follow Range.Normal", description = "Override for mobs' follow range on normal difficulty. The override is not applied if the mob's follow range is already higher than this. Set to 0 to not override Follow Range.")
-    public static Integer followRangeNormal = 28;
-    @Config(min = 0)
-    @Label(name = "Follow Range.Hard", description = "Override for mobs' follow range on hard difficulty. The override is not applied if the mob's follow range is already higher than this. Set to 0 to not override Follow Range.")
-    public static Integer followRangeHard = 32;
-    @Config(min = 0)
-    @Label(name = "Swim speed", description = "Bonus percentage health given to mobs on hard difficulty.")
-    public static Double swimSpeed = 2d;*/
     @Config
     @Label(name = "Enable DataPack", description = "Enables the DataPack that buffs mobs.")
     public static Boolean enableDataPack = true;
@@ -59,55 +24,13 @@ public class StatsBuffs extends Feature {
         IntegratedDataPacks.INTEGRATED_DATA_PACKS.add(new IntegratedDataPacks.IntegratedDataPack(PackType.SERVER_DATA, "mobs_buffs", net.minecraft.network.chat.Component.literal("Survival Reimagined Mobs Stats Buffs"), () -> this.isEnabled() && enableDataPack));
     }
 
-    /*@SubscribeEvent
-    public void onMobSpawn(EntityJoinLevelEvent event) {
-        if (!this.isEnabled()
-                || !(event.getEntity() instanceof Enemy)
-                || !(event.getEntity() instanceof LivingEntity entity))
-            return;
+    @Override
+    public void readConfig(ModConfigEvent event) {
+        super.readConfig(event);
+        Module.getFeature(Targeting.class).setConfigOption("Follow Range Override", new MinMax(0d));
 
-        CompoundTag persistentData = entity.getPersistentData();
-        if (persistentData.getBoolean(STAT_BUFFS_APPLIED))
-            return;
-
-        Difficulty difficulty = entity.level.getDifficulty();
-
-        float movementSpeedBonus = 0f;
-        float healthBonus = 0f;
-        float followRangeOverride = 0f;
-        switch (difficulty) {
-            case EASY -> {
-                movementSpeedBonus = movementSpeedEasy.floatValue();
-                healthBonus = healthEasy.floatValue();
-                followRangeOverride = followRangeEasy.floatValue();
-            }
-            case NORMAL -> {
-                movementSpeedBonus = movementSpeedNormal.floatValue();
-                healthBonus = healthNormal.floatValue();
-                followRangeOverride = followRangeNormal.floatValue();
-            }
-            case HARD -> {
-                movementSpeedBonus = movementSpeedHard.floatValue();
-                healthBonus = healthHard.floatValue();
-                followRangeOverride = followRangeHard.floatValue();
-            }
-        }
-
-        if (movementSpeedBonus != 0f) {
-            MCUtils.applyModifier(entity, Attributes.MOVEMENT_SPEED, MOVEMENT_SPEED_UUID, "Survival Reimagined Bonus Mov Speed", movementSpeedBonus, AttributeModifier.Operation.MULTIPLY_BASE, true);
-        }
-        if (healthBonus != 0f) {
-            MCUtils.applyModifier(entity, Attributes.MAX_HEALTH, HEALTH_UUID, "Survival Reimagined Bonus Health", healthBonus, AttributeModifier.Operation.MULTIPLY_BASE, true);
-        }
-        if (followRangeOverride != 0f) {
-            AttributeInstance attribute = entity.getAttribute(Attributes.FOLLOW_RANGE);
-            if (attribute != null && attribute.getBaseValue() < followRangeOverride)
-                MCUtils.setAttributeValue(entity, Attributes.FOLLOW_RANGE, followRangeOverride);
-        }
-        if (swimSpeed != 0f) {
-            MCUtils.applyModifier(entity, ForgeMod.SWIM_SPEED.get(), SWIM_SPEED_UUID, "Survival Reimagined Bonus Swim speed", swimSpeed, AttributeModifier.Operation.MULTIPLY_BASE, true);
-        }
-
-        persistentData.putBoolean(STAT_BUFFS_APPLIED, true);
-    }*/
+        Module.getFeature(Targeting.class).setConfigOption("XRay Range Override", new MinMax(0d));
+        //Read the config values
+        Module.getFeature(Targeting.class).readConfig(event);
+    }
 }
