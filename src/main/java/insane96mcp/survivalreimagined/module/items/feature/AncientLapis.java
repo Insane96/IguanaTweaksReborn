5ae1b3f473c6b88cf703ca9151d91874fef43e7b
Alberto <delvillano.alberto@gmail.com>
2023-04-05 17:27:04 +0200

    Started working on Ancient Lapis

---
+++ src/main/java/insane96mcp/survivalreimagined/module/items/feature/AncientLapis.java
@@ -0,0 +1,75 @@
+package insane96mcp.survivalreimagined.module.items.feature;
+
+import insane96mcp.insanelib.base.Feature;
+import insane96mcp.insanelib.base.Label;
+import insane96mcp.insanelib.base.Module;
+import insane96mcp.insanelib.base.config.LoadFeature;
+import insane96mcp.survivalreimagined.module.Modules;
+import insane96mcp.survivalreimagined.setup.SRItems;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraftforge.event.AnvilUpdateEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Label(name = "Ancient Lapis", description = "Add an item that increases the level of an enchantment, up to 1 level above the limit.")
+@LoadFeature(module = Modules.Ids.ITEMS)
+public class AncientLapis extends Feature {
+	public static final RegistryObject<Item> ANCIENT_LAPIS = SRItems.ITEMS.register("ancient_lapis", () -> new Item(new Item.Properties().stacksTo(1).fireResistant()));
+
+	public AncientLapis(Module module, boolean enabledByDefault, boolean canBeDisabled) {
+		super(module, enabledByDefault, canBeDisabled);
+	}
+
+	@SubscribeEvent
+	public void anvilUpdateEvent(final AnvilUpdateEvent event) {
+		if (!this.isEnabled())
+			return;
+
+		ItemStack left = event.getLeft();
+		if (!left.isEnchanted())
+			return;
+
+		ItemStack right = event.getRight();
+		if (!right.is(ANCIENT_LAPIS.get()))
+			return;
+
+		Map<Enchantment, Integer> allEnchantments = left.getAllEnchantments();
+		List<Enchantment> possibleEnchantments = new ArrayList<>();
+		for (var ench : allEnchantments.entrySet()) {
+			if (ench.getKey().getMaxLevel() <= 1
+					|| ench.getValue() >= ench.getKey().getMaxLevel() + 1)
+				continue;
+
+			possibleEnchantments.add(ench.getKey());
+		}
+		if (possibleEnchantments.isEmpty())
+			return;
+
+		RandomSource random = event.getPlayer().getRandom();
+		random.setSeed(event.getPlayer().getEnchantmentSeed());
+
+		Enchantment enchantmentChosen = possibleEnchantments.get(random.nextInt(possibleEnchantments.size()));
+
+		ItemStack result = left.copy();
+		result.removeTagKey("Enchantments");
+		for (var ench : allEnchantments.entrySet()) {
+			if (ench.getKey().equals(enchantmentChosen))
+				result.enchant(ench.getKey(), ench.getValue() + 1);
+			else
+				result.enchant(ench.getKey(), ench.getValue());
+		}
+		if (result.getBaseRepairCost() < 25)
+			result.setRepairCost(25);
+
+		event.setCost(25);
+		event.setMaterialCost(1);
+		event.setOutput(result);
+	}
+}
