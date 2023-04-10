423fe1c6fddca905fc8fb8fd1abc6b7f3017dd1e
Alberto Del Villano <delvillano.alberto@gmail.com>
2023-04-10 09:06:11 +0200

    Reduced Infinity cost

--- src/main/java/insane96mcp/survivalreimagined/mixin/ArrowInfiniteEnchantmentMixin.java
+++ src/main/java/insane96mcp/survivalreimagined/mixin/ArrowInfiniteEnchantmentMixin.java
@@ -7,14 +7,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ArrowInfiniteEnchantment.class)
-public class ArrowInfiniteEnchantmentMixin {
+public abstract class ArrowInfiniteEnchantmentMixin {
 	@Inject(at = @At("RETURN"), method = "getMinCost", cancellable = true)
 	private void onGetMinCost(int lvl, CallbackInfoReturnable<Integer> cir) {
-		cir.setReturnValue(12 + (lvl - 1) * 12);
+		cir.setReturnValue(lvl * 12);
 	}
 
 	@Inject(at = @At("RETURN"), method = "getMaxCost", cancellable = true)
 	private void onGetMaxCost(int lvl, CallbackInfoReturnable<Integer> cir) {
-		cir.setReturnValue(50);
+		cir.setReturnValue(65);
 	}
 }
