2f74455fb5bb860ba14f1dd8b1d04b75c4b6bf49
Insane96MCP <delvillano.alberto@gmail.com>
2023-04-16 11:50:19 +0200

    Less complicated Attributes mod for backwards slowdown

--- src/main/java/insane96mcp/survivalreimagined/module/movement/feature/BackwardsSlowdown.java
+++ src/main/java/insane96mcp/survivalreimagined/module/movement/feature/BackwardsSlowdown.java
@@ -18,7 +18,7 @@ import java.util.UUID;
 @LoadFeature(module = Modules.Ids.MOVEMENT)
 public class BackwardsSlowdown extends Feature {
 
-	private final UUID BACKWARD_WALK_SLOWDOWN = UUID.fromString("3c085336-5bee-465f-b4a8-6677f245e7fc");
+	private final UUID BACKWARD_WALK_SLOWDOWN_UUID = UUID.fromString("3c085336-5bee-465f-b4a8-6677f245e7fc");
 
 	@Config(min = 0d, max = 1d)
 	@Label(name = "Slowdown", description = "How much slower will the player go when walking backwards.")
@@ -36,20 +36,9 @@ public class BackwardsSlowdown extends Feature {
 				|| event.player.getAbilities().flying)
 			return;
 
-		AttributeModifier modifier = event.player.getAttribute(Attributes.MOVEMENT_SPEED).getModifier(BACKWARD_WALK_SLOWDOWN);
+		event.player.getAttribute(Attributes.MOVEMENT_SPEED).removeModifier(BACKWARD_WALK_SLOWDOWN_UUID);
 		if (event.player.zza < 0f) {
-			if (modifier == null) {
-				MCUtils.applyModifier(event.player, Attributes.MOVEMENT_SPEED, BACKWARD_WALK_SLOWDOWN, "backward slowdown", -slowdown, AttributeModifier.Operation.MULTIPLY_BASE, false);
-			}
-			else if (modifier.getAmount() != -slowdown) {
-				event.player.getAttribute(Attributes.MOVEMENT_SPEED).removeModifier(BACKWARD_WALK_SLOWDOWN);
-				MCUtils.applyModifier(event.player, Attributes.MOVEMENT_SPEED, BACKWARD_WALK_SLOWDOWN, "backward slowdown", -slowdown, AttributeModifier.Operation.MULTIPLY_BASE, false);
-			}
-		}
-		else {
-			if (modifier != null) {
-				event.player.getAttribute(Attributes.MOVEMENT_SPEED).removeModifier(BACKWARD_WALK_SLOWDOWN);
-			}
+			MCUtils.applyModifier(event.player, Attributes.MOVEMENT_SPEED, BACKWARD_WALK_SLOWDOWN_UUID, "Survival Reimagined backward slowdown", -slowdown, AttributeModifier.Operation.MULTIPLY_BASE, false);
 		}
 	}
 }
