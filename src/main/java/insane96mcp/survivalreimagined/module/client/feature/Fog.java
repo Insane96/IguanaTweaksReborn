16fd8f42403e089c3e138ab3bc7ae5fdf6776259
Insane96MCP <delvillano.alberto@gmail.com>
2023-04-21 15:40:33 +0200

    Fixed fog applying underwater

--- src/main/java/insane96mcp/survivalreimagined/module/client/feature/Fog.java
+++ src/main/java/insane96mcp/survivalreimagined/module/client/feature/Fog.java
@@ -15,6 +15,7 @@ import net.minecraft.world.level.material.FogType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ViewportEvent;
+import net.minecraftforge.common.ForgeMod;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import sereneseasons.api.season.SeasonHelper;
@@ -58,7 +59,7 @@ public class Fog extends Feature {
             return;
 
         Entity entity = event.getCamera().getEntity();
-        if (entity.level.dimension() == Level.OVERWORLD && entity.level.isRaining()) {
+        if (entity.getEyeInFluidType() == ForgeMod.EMPTY_TYPE.get() && entity.level.dimension() == Level.OVERWORLD && entity.level.isRaining()) {
             float renderDistance = Minecraft.getInstance().gameRenderer.getRenderDistance();
             float rainLevel = entity.level.getRainLevel(1f);
             float nearMultiplier = switch (SeasonHelper.getSeasonState(entity.level).getSeason()) {
