f61a04a856273cc435b452dd661da7e256e77e00
Insane96MCP <delvillano.alberto@gmail.com>
2023-04-25 21:59:29 +0200

    Increased desire paths chance

--- src/main/java/insane96mcp/survivalreimagined/mixin/AbstractArrowMixin.java
+++ src/main/java/insane96mcp/survivalreimagined/mixin/AbstractArrowMixin.java
@@ -4,7 +4,6 @@ import insane96mcp.insanelib.base.Feature;
 import insane96mcp.survivalreimagined.module.combat.feature.Stats;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.phys.EntityHitResult;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +12,6 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Debug(export = true)
 @Mixin(AbstractArrow.class)
 public abstract class AbstractArrowMixin {
     @Shadow public abstract boolean isCritArrow();
