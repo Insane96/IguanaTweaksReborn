3c16805737d156ca6996d341bcee8f9f50848d2a
Insane96MCP <delvillano.alberto@gmail.com>
2023-05-07 10:56:02 +0200

    Added attack invincibility config

--- src/main/java/insane96mcp/survivalreimagined/module/combat/feature/BetterAttackInvincibility.java
+++ src/main/java/insane96mcp/survivalreimagined/module/combat/feature/BetterAttackInvincibility.java
@@ -3,21 +3,27 @@ package insane96mcp.survivalreimagined.module.combat.feature;
 import insane96mcp.insanelib.base.Feature;
 import insane96mcp.insanelib.base.Label;
 import insane96mcp.insanelib.base.Module;
+import insane96mcp.insanelib.base.config.Config;
 import insane96mcp.insanelib.base.config.LoadFeature;
 import insane96mcp.survivalreimagined.event.PostEntityHurtEvent;
 import insane96mcp.survivalreimagined.module.Modules;
 import insane96mcp.survivalreimagined.network.NetworkHandler;
 import insane96mcp.survivalreimagined.network.message.SyncInvulnerableTimeMessage;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.TieredItem;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.network.NetworkDirection;
 
 @Label(name = "Better attack invincibility", description = "When attacking, less invincibility frames are applied to entities attacked based off attack speed (only works with a weapon).")
 @LoadFeature(module = Modules.Ids.COMBAT)
 public class BetterAttackInvincibility extends Feature {
+
+	@Config
+	@Label(name = "Weapons only", description = "If true less invincibility frames are applied to mobs only if using an item with attack speed modifier")
+	public static Boolean weaponsOnly = true;
+
 	public BetterAttackInvincibility(Module module, boolean enabledByDefault, boolean canBeDisabled) {
 		super(module, enabledByDefault, canBeDisabled);
 	}
@@ -27,7 +33,7 @@ public class BetterAttackInvincibility extends Feature {
 		if (!this.isEnabled()
 				|| !(event.getDamageSource().getEntity() instanceof ServerPlayer serverPlayer)
 				|| serverPlayer.getAttribute(Attributes.ATTACK_SPEED).getValue() < 2f
-				|| !(serverPlayer.getMainHandItem().getItem() instanceof TieredItem))
+				|| (weaponsOnly && serverPlayer.getMainHandItem().getAttributeModifiers(EquipmentSlot.MAINHAND).isEmpty()))
 			return;
 
 		int time = (int) ((1f / serverPlayer.getAttribute(Attributes.ATTACK_SPEED).getValue()) * 20);
