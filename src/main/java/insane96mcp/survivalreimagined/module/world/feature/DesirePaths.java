72db09a54c9dc171780d6605af66ade102f6fabb
Insane96MCP <delvillano.alberto@gmail.com>
2023-05-21 08:52:56 +0200

    Rarer desire paths

--- src/main/java/insane96mcp/survivalreimagined/module/world/feature/DesirePaths.java
+++ src/main/java/insane96mcp/survivalreimagined/module/world/feature/DesirePaths.java
@@ -2,7 +2,6 @@ package insane96mcp.survivalreimagined.module.world.feature;
 
 import insane96mcp.insanelib.base.Feature;
 import insane96mcp.insanelib.base.Label;
-import insane96mcp.insanelib.base.Module;
 import insane96mcp.insanelib.base.config.Config;
 import insane96mcp.insanelib.base.config.LoadFeature;
 import insane96mcp.survivalreimagined.SurvivalReimagined;
@@ -36,7 +35,7 @@ public class DesirePaths extends Feature {
 
 	@Config(min = 0d, max = 1d)
 	@Label(name = "Chance to transform", description = "Chance for blocks to transform each tick")
-	public static Double chanceToTransform = 0.02d;
+	public static Double chanceToTransform = 0.01d;
 	@Config
 	@Label(name = "Break Tall Grass", description = "Tall grass is broken when grass is transformed")
 	public static Boolean breakTallGrass = true;
