cff15ef2705bbefe59a3b0a2c306deb4f12cd141
Insane96MCP <delvillano.alberto@gmail.com>
2023-05-21 20:34:24 +0200

    Heavily nerfed drowned trident's and slightly nerfed skeleton's damages

--- src/main/java/insane96mcp/survivalreimagined/mixin/AbstractArrowMixin.java
+++ src/main/java/insane96mcp/survivalreimagined/mixin/AbstractArrowMixin.java
@@ -2,7 +2,11 @@ package insane96mcp.survivalreimagined.mixin;
 
 import insane96mcp.insanelib.base.Feature;
 import insane96mcp.survivalreimagined.module.combat.feature.Stats;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,8 +17,14 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractArrow.class)
-public abstract class AbstractArrowMixin {
+public abstract class AbstractArrowMixin extends Projectile {
+    protected AbstractArrowMixin(EntityType<? extends Projectile> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
+
     @Shadow public abstract boolean isCritArrow();
+
+    @Shadow public abstract void setBaseDamage(double pBaseDamage);
 
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z", shift = At.Shift.AFTER), method = "onHitEntity")
     private void onHitEntity(EntityHitResult entityHitResult, CallbackInfo ci) {
@@ -36,5 +46,10 @@ public abstract class AbstractArrowMixin {
         if (Feature.isEnabled(Stats.class) && Stats.disableCritArrowsBonusDamage)
             return false;
         return this.isCritArrow();
+    }
+
+    @Inject(method = "setEnchantmentEffectsFromEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/projectile/AbstractArrow;setBaseDamage(D)V", ordinal = 0, shift = At.Shift.AFTER))
+    private void onSetMobArrowDamage(LivingEntity pShooter, float pVelocity, CallbackInfo ci) {
+        this.setBaseDamage(pVelocity * 2.0F);
     }
 }
