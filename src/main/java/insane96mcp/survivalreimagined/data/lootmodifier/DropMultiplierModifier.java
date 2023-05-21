bcead426d61303e31e2e52c543684a5ac6ba21a6
Insane96MCP <delvillano.alberto@gmail.com>
2023-05-21 14:16:11 +0200

    Fixed saplings no longer dropping from ... saplings

--- src/main/java/insane96mcp/survivalreimagined/data/lootmodifier/DropMultiplierModifier.java
+++ src/main/java/insane96mcp/survivalreimagined/data/lootmodifier/DropMultiplierModifier.java
@@ -47,6 +47,7 @@ public class DropMultiplierModifier extends LootModifier {
     private float multiplier;
     //This amount is subtracted from the total amount before applying the multiplier
     private int amountToKeep = 0;
+    //This amount is subtracted from the total amount before applying the multiplier
 
     public DropMultiplierModifier(LootItemCondition[] conditionsIn, Optional<Item> item, Optional<TagKey<Item>> tag, float multiplier, int amountToKeep) {
         super(conditionsIn);
