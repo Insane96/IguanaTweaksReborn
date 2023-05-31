bb09bb2c746d97aade12d1639583364efd518003
Alberto <delvillano.alberto@gmail.com>
2023-05-31 14:14:00 +0200

    Fixed elytra nerf

--- src/main/java/insane96mcp/survivalreimagined/module/movement/feature/ElytraNerf.java
+++ src/main/java/insane96mcp/survivalreimagined/module/movement/feature/ElytraNerf.java
@@ -6,7 +6,9 @@ import insane96mcp.insanelib.base.Module;
 import insane96mcp.insanelib.base.config.Config;
 import insane96mcp.insanelib.base.config.LoadFeature;
 import insane96mcp.survivalreimagined.module.Modules;
+import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.TickEvent;
@@ -18,7 +20,7 @@ public class ElytraNerf extends Feature {
 
 	@Config(min = 0d, max = 1d)
 	@Label(name = "Strength", description = "How much the player is pulled down when flying with elytra in non-end dimensions.")
-	public static Double pullStrength = 0.05d;
+	public static Double pullStrength = 0.3d;
 
 	public ElytraNerf(Module module, boolean enabledByDefault, boolean canBeDisabled) {
 		super(module, enabledByDefault, canBeDisabled);
@@ -28,11 +30,11 @@ public class ElytraNerf extends Feature {
 	@SubscribeEvent
 	public void onPlayerTick(TickEvent.PlayerTickEvent event) {
 		if (!this.isEnabled()
-				|| event.phase == TickEvent.Phase.START
+				|| event.phase != TickEvent.Phase.START
 				|| !event.player.isFallFlying()
 				|| event.player.level.dimension() == Level.END)
 			return;
 
-		event.player.getDeltaMovement().add(0d, -pullStrength, 0d);
+		event.player.move(MoverType.SELF, new Vec3(0, -pullStrength, 0));
 	}
 }
