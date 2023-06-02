781741db235305c1585f5be799079298881e52a7
Insane96MCP <delvillano.alberto@gmail.com>
2023-06-02 08:59:09 +0200

    Better explosive arrows

--- src/main/java/insane96mcp/survivalreimagined/module/combat/entity/projectile/ExplosiveArrow.java
+++ src/main/java/insane96mcp/survivalreimagined/module/combat/entity/projectile/ExplosiveArrow.java
@@ -1,11 +1,15 @@
 package insane96mcp.survivalreimagined.module.combat.entity.projectile;
 
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.Arrow;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.Vec3;
 
 public class ExplosiveArrow extends Arrow {
     public ExplosiveArrow(EntityType<? extends Arrow> pEntityType, Level pLevel) {
@@ -20,20 +24,66 @@ public class ExplosiveArrow extends Arrow {
         super(pLevel, pShooter);
     }
 
-    //TODO overwrite both to have more control
     @Override
     protected void onHitBlock(BlockHitResult pResult) {
-        super.onHitBlock(pResult);
+        Vec3 vec3 = pResult.getLocation().subtract(this.getX(), this.getY(), this.getZ());
+        this.setDeltaMovement(vec3);
+        Vec3 vec31 = vec3.normalize().scale(0.05F);
+        this.setPosRaw(this.getX() - vec31.x, this.getY() - vec31.y, this.getZ() - vec31.z);
         this.discard();
         if (!this.level.isClientSide)
-            this.level.explode(this, this.getX(), this.getY(), this.getZ(), 2f, Level.ExplosionInteraction.MOB);
+            this.level.explode(this, this.getX(), this.getY(), this.getZ(), 2f, Level.ExplosionInteraction.BLOCK);
     }
 
     @Override
     protected void onHitEntity(EntityHitResult pResult) {
-        super.onHitEntity(pResult);
-        this.discard();
-        if (!this.level.isClientSide)
-            this.level.explode(this, this.getX(), this.getY(), this.getZ(), 2f, Level.ExplosionInteraction.MOB);
+        Entity entityHit = pResult.getEntity();
+        /*if (this.getPierceLevel() > 0) {
+            if (this.piercingIgnoreEntityIds == null) {
+                this.piercingIgnoreEntityIds = new IntOpenHashSet(5);
+            }
+
+            if (this.piercedAndKilledEntities == null) {
+                this.piercedAndKilledEntities = Lists.newArrayListWithCapacity(5);
+            }
+
+            if (this.piercingIgnoreEntityIds.size() >= this.getPierceLevel() + 1) {
+                this.discard();
+                return;
+            }
+
+            this.piercingIgnoreEntityIds.add(entityHit.getId());
+        }*/
+
+        Entity owner = this.getOwner();
+        DamageSource damagesource;
+        if (owner == null) {
+            damagesource = this.damageSources().arrow(this, this);
+        }
+        else {
+            damagesource = this.damageSources().arrow(this, owner);
+            if (owner instanceof LivingEntity) {
+                ((LivingEntity)owner).setLastHurtMob(entityHit);
+            }
+        }
+
+        boolean isEnderman = entityHit.getType() == EntityType.ENDERMAN;
+        if (!isEnderman && entityHit.hurt(damagesource, 0.01f)) {
+            this.discard();
+            if (!this.level.isClientSide)
+                this.level.explode(this, this.getX(), this.getY(), this.getZ(), 2f, Level.ExplosionInteraction.BLOCK);
+        }
+        else {
+            this.setDeltaMovement(this.getDeltaMovement().scale(-0.1D));
+            this.setYRot(this.getYRot() + 180.0F);
+            this.yRotO += 180.0F;
+            if (!this.level.isClientSide && this.getDeltaMovement().lengthSqr() < 1.0E-7D) {
+                if (this.pickup == AbstractArrow.Pickup.ALLOWED) {
+                    this.spawnAtLocation(this.getPickupItem(), 0.1F);
+                }
+
+                this.discard();
+            }
+        }
     }
 }
