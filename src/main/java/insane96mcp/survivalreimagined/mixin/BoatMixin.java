bd93b24da1262b42c42193f476e7201f5bca8308
Alberto <delvillano.alberto@gmail.com>
2023-06-21 09:35:17 +0200

    Boats can now move horses?

--- src/main/java/insane96mcp/survivalreimagined/mixin/BoatMixin.java
+++ src/main/java/insane96mcp/survivalreimagined/mixin/BoatMixin.java
@@ -1,6 +1,8 @@
 package insane96mcp.survivalreimagined.mixin;
 
 import insane96mcp.survivalreimagined.module.movement.feature.Boats;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.animal.horse.AbstractHorse;
 import net.minecraft.world.entity.vehicle.Boat;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,7 +12,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Boat.class)
 public abstract class BoatMixin {
 	@Inject(at = @At("RETURN"), method = "getGroundFriction", cancellable = true)
-	public void getGroundFriction(CallbackInfoReturnable<Float> callbackInfo) {
+	public void onGetGroundFriction(CallbackInfoReturnable<Float> callbackInfo) {
 		callbackInfo.setReturnValue(Boats.getBoatFriction(callbackInfo.getReturnValueF()));
+	}
+
+	@Inject(at = @At("RETURN"), method = "hasEnoughSpaceFor", cancellable = true)
+	public void onHasEnoughSpaceFor(Entity other, CallbackInfoReturnable<Boolean> cir) {
+		cir.setReturnValue(cir.getReturnValueZ() || other instanceof AbstractHorse);
 	}
 }
