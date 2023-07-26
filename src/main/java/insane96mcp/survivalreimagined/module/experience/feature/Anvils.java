650adef0f88d4266cc973d45dbb65f0cabf2faa1
Insane96 <delvillano.alberto@gmail.com>
2023-07-26 12:23:03 +0200

    Repairable soul steel and keego with diamonds

--- src/main/java/insane96mcp/survivalreimagined/module/experience/feature/Anvils.java
+++ src/main/java/insane96mcp/survivalreimagined/module/experience/feature/Anvils.java
@@ -69,9 +69,15 @@ public class Anvils extends SRFeature {
     @Label(name = "Partially repair Smithed items with base material", description = "E.g. Netherite Tools can be repaired up to 75% of max durability with Diamonds. More items/repair item combination can be added in the anvil_partial_repair_items.json file")
     public static Boolean partiallyRepairUpgradedItemsWithBaseMaterial = true;
     public static final ArrayList<TwinIdTagMatcher> PARTIAL_REPAIR_ITEMS = new ArrayList<>(List.of(
-            new TwinIdTagMatcher(IdTagMatcher.Type.TAG, "survivalreimagined:equipment/hand/netherite", IdTagMatcher.Type.ID,"survivalreimagined:soul_steel_ingot"),
-            new TwinIdTagMatcher(IdTagMatcher.Type.TAG, "survivalreimagined:equipment/armor/netherite", IdTagMatcher.Type.ID,"survivalreimagined:soul_steel_ingot"),
-            new TwinIdTagMatcher(IdTagMatcher.Type.ID, "shieldsplus:netherite_shield", IdTagMatcher.Type.ID,"survivalreimagined:soul_steel_ingot"),
+            new TwinIdTagMatcher(IdTagMatcher.Type.TAG, "survivalreimagined:equipment/hand/netherite", IdTagMatcher.Type.ID,"minecraft:diamond"),
+            new TwinIdTagMatcher(IdTagMatcher.Type.TAG, "survivalreimagined:equipment/armor/netherite", IdTagMatcher.Type.ID,"minecraft:diamond"),
+            new TwinIdTagMatcher(IdTagMatcher.Type.ID, "shieldsplus:netherite_shield", IdTagMatcher.Type.ID,"minecraft:diamond"),
+            new TwinIdTagMatcher(IdTagMatcher.Type.TAG, "survivalreimagined:equipment/hand/soul_steel", IdTagMatcher.Type.ID,"minecraft:diamond"),
+            new TwinIdTagMatcher(IdTagMatcher.Type.TAG, "survivalreimagined:equipment/armor/soul_steel", IdTagMatcher.Type.ID,"minecraft:diamond"),
+            new TwinIdTagMatcher(IdTagMatcher.Type.ID, "survivalreimagined:soul_steel_shield", IdTagMatcher.Type.ID,"minecraft:diamond"),
+            new TwinIdTagMatcher(IdTagMatcher.Type.TAG, "survivalreimagined:equipment/hand/keego", IdTagMatcher.Type.ID,"minecraft:diamond"),
+            new TwinIdTagMatcher(IdTagMatcher.Type.TAG, "survivalreimagined:equipment/armor/keego", IdTagMatcher.Type.ID,"minecraft:diamond"),
+            new TwinIdTagMatcher(IdTagMatcher.Type.ID, "survivalreimagined:keego_shield", IdTagMatcher.Type.ID,"minecraft:diamond"),
             new TwinIdTagMatcher(IdTagMatcher.Type.TAG, "survivalreimagined:equipment/hand/durium", IdTagMatcher.Type.ID,"minecraft:iron_ingot"),
             new TwinIdTagMatcher(IdTagMatcher.Type.TAG, "survivalreimagined:equipment/armor/durium", IdTagMatcher.Type.ID,"minecraft:iron_ingot"),
             new TwinIdTagMatcher(IdTagMatcher.Type.ID, "survivalreimagined:durium_shield", IdTagMatcher.Type.ID,"minecraft:iron_ingot"),
