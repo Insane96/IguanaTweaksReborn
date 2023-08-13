5e4ea72308f37ad848e1e3f41dc04f1bffa5f08d
Alberto Del Villano <delvillano.alberto@gmail.com>
2023-08-13 18:51:39 +0200

    Fixed TNT not dropping all the blocks

--- src/main/java/insane96mcp/survivalreimagined/setup/SRLootItemConditions.java
+++ src/main/java/insane96mcp/survivalreimagined/setup/SRLootItemConditions.java
@@ -3,6 +3,7 @@ package insane96mcp.survivalreimagined.setup;
 import insane96mcp.survivalreimagined.SurvivalReimagined;
 import insane96mcp.survivalreimagined.data.condition.BlockTagCondition;
 import insane96mcp.survivalreimagined.data.condition.LootItemCurrentSeasonCondition;
+import insane96mcp.survivalreimagined.data.condition.NonPlayerArisedDropCondition;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,5 +13,6 @@ public class SRLootItemConditions {
     public static final DeferredRegister<LootItemConditionType> REGISTRY = DeferredRegister.create(Registries.LOOT_CONDITION_TYPE.location(), SurvivalReimagined.MOD_ID);
 
     public static final RegistryObject<LootItemConditionType> CURRENT_SEASON = REGISTRY.register("current_season", () -> new LootItemConditionType(new LootItemCurrentSeasonCondition.Serializer()));
+    public static final RegistryObject<LootItemConditionType> NON_PLAYER_ARISED_DROP = REGISTRY.register("non_player_arised_drop", () -> new LootItemConditionType(new NonPlayerArisedDropCondition.Serializer()));
     public static final RegistryObject<LootItemConditionType> BLOCK_TAG_MATCH = REGISTRY.register("block_tag_match", () -> new LootItemConditionType(new BlockTagCondition.Serializer()));
 }
