4717a67434c1938be4bb93a0d8eac4295ea7c507
Insane96 <delvillano.alberto@gmail.com>
2023-09-02 22:01:21 +0200

    Doubled the bell range to glow raiders

---
+++ src/main/java/insane96mcp/survivalreimagined/mixin/BellBlockEntityMixin.java
@@ -0,0 +1,20 @@
+package insane96mcp.survivalreimagined.mixin;
+
+import net.minecraft.world.level.block.entity.BellBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(BellBlockEntity.class)
+public class BellBlockEntityMixin {
+
+	@ModifyConstant(method = "updateEntities", constant = @Constant(doubleValue = 48))
+	private static double nearbyEntitiesRange(double range) {
+		return 96d;
+	}
+
+	@ModifyConstant(method = "isRaiderWithinRange", constant = @Constant(doubleValue = 48))
+	private static double nearbyRiderRange(double range) {
+		return 96d;
+	}
+}
