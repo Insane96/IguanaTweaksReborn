64ac5ba0eaf3965b2d073b6e04e365ae2b86f776
Insane96 <delvillano.alberto@gmail.com>
2023-09-14 15:26:35 +0200

    Smarter removal of wooden tools

--- src/main/java/insane96mcp/survivalreimagined/data/generator/SRGlobalLootModifierProvider.java
+++ src/main/java/insane96mcp/survivalreimagined/data/generator/SRGlobalLootModifierProvider.java
@@ -2,7 +2,6 @@ package insane96mcp.survivalreimagined.data.generator;
 
 import insane96mcp.survivalreimagined.module.experience.Lapis;
 import insane96mcp.survivalreimagined.module.farming.crops.Crops;
-import insane96mcp.survivalreimagined.module.items.flintexpansion.FlintExpansion;
 import insane96mcp.survivalreimagined.module.items.recallidol.RecallIdol;
 import insane96mcp.survivalreimagined.module.mobs.equipment.Equipment;
 import insane96mcp.survivalreimagined.module.movement.minecarts.Minecarts;
@@ -22,7 +21,6 @@ public class SRGlobalLootModifierProvider extends GlobalLootModifierProvider {
         Crops.addGlobalLoot(this);
         Equipment.addGlobalLoot(this);
         Loot.addGlobalLoot(this);
-        FlintExpansion.addGlobalLoot(this);
         Minecarts.addGlobalLoot(this);
         CoalFire.addGlobalLoot(this);
         Lapis.addGlobalLoot(this);
--- src/main/java/insane96mcp/survivalreimagined/module/items/flintexpansion/FlintExpansion.java
+++ src/main/java/insane96mcp/survivalreimagined/module/items/flintexpansion/FlintExpansion.java
@@ -8,33 +8,19 @@ import insane96mcp.insanelib.base.config.LoadFeature;
 import insane96mcp.insanelib.item.ILItemTier;
 import insane96mcp.shieldsplus.world.item.SPShieldItem;
 import insane96mcp.shieldsplus.world.item.SPShieldMaterial;
-import insane96mcp.survivalreimagined.SurvivalReimagined;
 import insane96mcp.survivalreimagined.base.SimpleBlockWithItem;
-import insane96mcp.survivalreimagined.data.lootmodifier.ReplaceLootModifier;
 import insane96mcp.survivalreimagined.module.Modules;
-import insane96mcp.survivalreimagined.module.items.ItemStats;
 import insane96mcp.survivalreimagined.module.misc.DataPacks;
 import insane96mcp.survivalreimagined.setup.IntegratedDataPack;
 import insane96mcp.survivalreimagined.setup.SRRegistries;
-import insane96mcp.survivalreimagined.utils.Utils;
-import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.MapColor;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.common.data.GlobalLootModifierProvider;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.RegistryObject;
 
 @Label(name = "Flint Expansion", description = "Add flint tools and make wooden tools useless. Also add flint blocks.")
@@ -58,92 +44,11 @@ public class FlintExpansion extends Feature {
 	public static final SimpleBlockWithItem POLISHED_FLINT_BLOCK = SimpleBlockWithItem.register("polished_flint_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(1.5F, 5.0F)));
 
 	@Config
-	@Label(name = "Disable Wooden Tools", description = "Makes wooden items deal no damage and not able to break blocks.")
-	public static Boolean disableWoodenTools = true;
-
-	@Config
-	@Label(name = "Disable Wooden Tools Recipe", description = "Disable wooden tools recipe.")
+	@Label(name = "Disable Wooden Tools + Recipes", description = "Makes wooden items deal no damage and not able to break blocks and disables the recipe.")
 	public static Boolean disableWoodenToolsRecipe = true;
 
 	public FlintExpansion(Module module, boolean enabledByDefault, boolean canBeDisabled) {
 		super(module, enabledByDefault, canBeDisabled);
 		IntegratedDataPack.INTEGRATED_DATA_PACKS.add(new IntegratedDataPack(PackType.SERVER_DATA, "disable_wooden_tools", Component.literal("Survival Reimagined Disable Wooden Tools"), () -> this.isEnabled() && !DataPacks.disableAllDataPacks && disableWoodenToolsRecipe));
-	}
-
-	//TODO Wtf is this, use a datapack
-	@SubscribeEvent
-	public void disableWoodenTools(PlayerEvent.BreakSpeed event) {
-		if (!this.isEnabled()
-			|| !disableWoodenTools)
-			return;
-
-		Player player = event.getEntity();
-		if (isWoodenTool(player.getMainHandItem().getItem())) {
-			event.setCanceled(true);
-			event.getEntity().displayClientMessage(Component.translatable(ItemStats.NO_EFFICIENCY_ITEM), true);
-		}
-	}
-
-	@SubscribeEvent
-	public void processAttackDamage(LivingHurtEvent event) {
-		if (!this.isEnabled()
-				|| !disableWoodenTools)
-			return;
-
-		if (isWoodenTool(event.getEntity().getMainHandItem().getItem())) {
-			event.setAmount(1f);
-			if (event.getEntity() instanceof Player player)
-				player.displayClientMessage(Component.translatable(ItemStats.NO_DAMAGE_ITEM), true);
-		}
-	}
-
-	private static final String path = "flint_expansion/";
-
-	public static void addGlobalLoot(GlobalLootModifierProvider provider) {
-		provider.add(path + "replace_wooden_sword", new ReplaceLootModifier.Builder(Items.WOODEN_SWORD, SWORD.get())
-				.applyToChestsOnly()
-				.keepDurability()
-				.build()
-		);
-		provider.add(path + "replace_wooden_axe", new ReplaceLootModifier.Builder(Items.WOODEN_AXE, AXE.get())
-				.applyToChestsOnly()
-				.keepDurability()
-				.build()
-		);
-		provider.add(path + "replace_wooden_shovel", new ReplaceLootModifier.Builder(Items.WOODEN_SHOVEL, SHOVEL.get())
-				.applyToChestsOnly()
-				.keepDurability()
-				.build()
-		);
-		provider.add(path + "replace_wooden_pickaxe", new ReplaceLootModifier.Builder(Items.WOODEN_PICKAXE, PICKAXE.get())
-				.applyToChestsOnly()
-				.keepDurability()
-				.build()
-		);
-		provider.add(path + "replace_wooden_hoe", new ReplaceLootModifier.Builder(Items.WOODEN_HOE, HOE.get())
-				.applyToChestsOnly()
-				.keepDurability()
-				.build()
-		);
-	}
-
-	@OnlyIn(Dist.CLIENT)
-	@SubscribeEvent
-	public void onTooltip(ItemTooltipEvent event) {
-		if (!this.isEnabled()
-				|| !disableWoodenTools)
-			return;
-
-		if (isWoodenTool(event.getItemStack().getItem())) {
-			event.getToolTip().add(Component.translatable(ItemStats.NO_DAMAGE_ITEM).withStyle(ChatFormatting.RED));
-			event.getToolTip().add(Component.translatable(ItemStats.NO_EFFICIENCY_ITEM).withStyle(ChatFormatting.RED));
-		}
-		else if (event.getItemStack().getItem().equals(Items.WOODEN_SWORD)) {
-			event.getToolTip().add(Component.translatable(ItemStats.NO_DAMAGE_ITEM).withStyle(ChatFormatting.RED));
-		}
-	}
-
-	private static boolean isWoodenTool(Item item) {
-		return Utils.isItemInTag(item, new ResourceLocation(SurvivalReimagined.MOD_ID, "equipment/hand/tools/wooden"));
 	}
 }
