4b3c81faf15fb24e9eca2da8c05043bfc39dd5c5
Alberto Del Villano <delvillano.alberto@gmail.com>
2023-12-29 12:40:59 +0100

    Removed new enchantments. Will be in Survival Reimagined

--- src/main/java/insane96mcp/iguanatweaksreborn/module/experience/enchantments/EnchantmentsFeature.java
+++ src/main/java/insane96mcp/iguanatweaksreborn/module/experience/enchantments/EnchantmentsFeature.java
@@ -1,13 +1,10 @@
 package insane96mcp.iguanatweaksreborn.module.experience.enchantments;
 
-import com.mojang.blaze3d.platform.InputConstants;
 import insane96mcp.iguanatweaksreborn.IguanaTweaksReborn;
 import insane96mcp.iguanatweaksreborn.module.Modules;
 import insane96mcp.iguanatweaksreborn.module.combat.stats.Stats;
-import insane96mcp.iguanatweaksreborn.module.experience.enchantments.enchantment.*;
-import insane96mcp.iguanatweaksreborn.network.message.JumpMidAirMessage;
+import insane96mcp.iguanatweaksreborn.module.experience.enchantments.enchantment.BaneOfSSSS;
 import insane96mcp.iguanatweaksreborn.setup.ITRRegistries;
-import insane96mcp.iguanatweaksreborn.utils.Utils;
 import insane96mcp.insanelib.base.Feature;
 import insane96mcp.insanelib.base.JsonFeature;
 import insane96mcp.insanelib.base.Label;
@@ -15,14 +12,8 @@ import insane96mcp.insanelib.base.Module;
 import insane96mcp.insanelib.base.config.Config;
 import insane96mcp.insanelib.base.config.LoadFeature;
 import insane96mcp.insanelib.data.IdTagMatcher;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.effect.MobEffectUtil;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.projectile.AbstractArrow;
@@ -30,20 +21,10 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.HitResult;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.InputEvent;
-import net.minecraftforge.client.event.RenderLevelStageEvent;
 import net.minecraftforge.common.ForgeMod;
-import net.minecraftforge.event.ItemAttributeModifierEvent;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
-import net.minecraftforge.event.entity.living.*;
-import net.minecraftforge.event.entity.player.CriticalHitEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.level.BlockEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 import net.minecraftforge.registries.RegistryObject;
@@ -51,26 +32,10 @@ import net.minecraftforge.registries.RegistryObject;
 import java.util.ArrayList;
 import java.util.List;
 
-@Label(name = "Enchantments", description = "Change some enchantments related stuff and adds new enchantments.")
+@Label(name = "Enchantments", description = "Changes to some enchantments related stuff.")
 @LoadFeature(module = Modules.Ids.EXPERIENCE)
 public class EnchantmentsFeature extends JsonFeature {
-
-	public static final TagKey<EntityType<?>> WATER_COOLANT_AFFECTED = TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation(IguanaTweaksReborn.MOD_ID, "water_coolant_affected"));
-	public static final RegistryObject<Enchantment> MAGNETIC = ITRRegistries.ENCHANTMENTS.register("magnetic", Magnetic::new);
-	public static final RegistryObject<Enchantment> MAGIC_PROTECTION = ITRRegistries.ENCHANTMENTS.register("magic_protection", MagicProtection::new);
-	public static final RegistryObject<Enchantment> MELEE_PROTECTION = ITRRegistries.ENCHANTMENTS.register("melee_protection", MeleeProtection::new);
-	public static final RegistryObject<Enchantment> BLASTING = ITRRegistries.ENCHANTMENTS.register("blasting", Blasting::new);
-	public static final RegistryObject<Enchantment> EXPANDED = ITRRegistries.ENCHANTMENTS.register("expanded", Expanded::new);
-	public static final RegistryObject<Enchantment> STEP_UP = ITRRegistries.ENCHANTMENTS.register("step_up", StepUp::new);
 	public static final RegistryObject<Enchantment> BANE_OF_SSSSS = ITRRegistries.ENCHANTMENTS.register("bane_of_sssss", BaneOfSSSS::new);
-	public static final RegistryObject<Enchantment> WATER_COOLANT = ITRRegistries.ENCHANTMENTS.register("water_coolant", WaterCoolant::new);
-	public static final RegistryObject<Enchantment> SMARTNESS = ITRRegistries.ENCHANTMENTS.register("smartness", Smartness::new);
-	public static final RegistryObject<Enchantment> MA_JUMP = ITRRegistries.ENCHANTMENTS.register("ma_jump", DoubleJump::new);
-	public static final RegistryObject<Enchantment> GRAVITY_DEFYING = ITRRegistries.ENCHANTMENTS.register("gravity_defying", GravityDefying::new);
-	public static final RegistryObject<Enchantment> CRITICAL = ITRRegistries.ENCHANTMENTS.register("critical", Critical::new);
-	public static final RegistryObject<Enchantment> SWIFT_STRIKE = ITRRegistries.ENCHANTMENTS.register("swift_strike", SwiftStrike::new);
-	public static final RegistryObject<Enchantment> HEALTHY = ITRRegistries.ENCHANTMENTS.register("healthy", Healthy::new);
-
 	@Config
 	@Label(name = "Infinity overhaul", description = "Infinity can go up to level 4. Each level makes an arrow have 1 in level+1 chance to not consume.")
 	public static Boolean infinityOverhaul = true;
@@ -99,7 +64,7 @@ public class EnchantmentsFeature extends JsonFeature {
 	public static final ArrayList<IdTagMatcher> DISABLED_ENCHANTMENTS_DEFAULT = new ArrayList<>(List.of(
 			IdTagMatcher.newId("minecraft:protection"),
 			IdTagMatcher.newId("minecraft:mending"),
-			IdTagMatcher.newId("minecraft:bane_of_arthropods"),
+			// IdTagMatcher.newId("minecraft:bane_of_arthropods"),
 			IdTagMatcher.newId("allurement:reforming"),
 			IdTagMatcher.newId("allurement:alleviating")
 
@@ -175,81 +140,6 @@ public class EnchantmentsFeature extends JsonFeature {
 		return Feature.isEnabled(EnchantmentsFeature.class) && thornsOverhaul;
 	}
 
-    @SubscribeEvent
-	public void onAttributeModifiers(ItemAttributeModifierEvent event) {
-		if (!this.isEnabled())
-			return;
-
-		StepUp.applyAttributeModifier(event);
-		GravityDefying.applyAttributeModifier(event);
-		Healthy.applyAttributeModifier(event);
-		SwiftStrike.applyAttributeModifier(event);
-		MeleeProtection.applyAttributeModifier(event);
-	}
-
-	@SubscribeEvent
-	public void onEntityTick(LivingEvent.LivingTickEvent event) {
-		if (!this.isEnabled())
-			return;
-
-		Magnetic.tryPullItems(event.getEntity());
-	}
-
-	@SubscribeEvent
-	public void onEffectAdded(MobEffectEvent.Added event) {
-		if (!this.isEnabled())
-			return;
-
-		MagicProtection.reduceBadEffectsDuration(event.getEntity(), event.getEffectInstance());
-	}
-
-	@SubscribeEvent
-	public void onBreakSpeed(PlayerEvent.BreakSpeed event) {
-		if (!this.isEnabled())
-			return;
-
-		event.setNewSpeed(event.getNewSpeed() + Blasting.getMiningSpeedBoost(event.getEntity(), event.getState()));
-	}
-
-	@SubscribeEvent
-	public void onExperienceDropped(LivingExperienceDropEvent event) {
-		if (!this.isEnabled()
-				|| event.getAttackingPlayer() == null)
-			return;
-		int lvl = EnchantmentHelper.getEnchantmentLevel(SMARTNESS.get(), event.getAttackingPlayer());
-		if (lvl > 0)
-			event.setDroppedExperience(Smartness.getIncreasedExperience(event.getEntity().getRandom(), lvl, event.getDroppedExperience()));
-	}
-
-	@SubscribeEvent(priority = EventPriority.LOWEST)
-	public void onBlockExperienceDropped(BlockEvent.BreakEvent event) {
-		if (!this.isEnabled())
-			return;
-		int lvl = EnchantmentHelper.getEnchantmentLevel(SMARTNESS.get(), event.getPlayer());
-		if (lvl > 0)
-			event.setExpToDrop(Smartness.getIncreasedExperience(event.getLevel().getRandom(), lvl, event.getExpToDrop()));
-	}
-
-	@SubscribeEvent
-	@OnlyIn(Dist.CLIENT)
-	public void onRenderLevel(RenderLevelStageEvent event) {
-		if (!this.isEnabled())
-			return;
-
-		Expanded.applyDestroyAnimation(event);
-	}
-
-	//Priority high: run before Timber Trees
-	@SubscribeEvent(priority = EventPriority.HIGH)
-	public void onBlockBreak(BlockEvent.BreakEvent event) {
-		if (!this.isEnabled())
-			return;
-
-		HitResult pick = event.getPlayer().pick(event.getPlayer().getEntityReach() + 0.5d, 1f, false);
-		if (pick instanceof BlockHitResult blockHitResult)
-			Expanded.apply(event.getPlayer(), event.getPlayer().level(), event.getPos(), blockHitResult.getDirection(), event.getState());
-	}
-
 	@SubscribeEvent
 	public void onLivingAttack(LivingHurtEvent event) {
 		if (!this.isEnabled()
@@ -257,44 +147,6 @@ public class EnchantmentsFeature extends JsonFeature {
 			return;
 
 		baneOfSssssOnAttack(attacker, event.getEntity(), event);
-		waterCoolantOnAttack(attacker, event.getEntity(), event);
-	}
-
-	@SubscribeEvent(priority = EventPriority.HIGH)
-	public void onCriticalHit(CriticalHitEvent event) {
-		if (!this.isEnabled())
-			return;
-
-		int lvl = event.getEntity().getMainHandItem().getEnchantmentLevel(CRITICAL.get());
-		if (lvl <= 0)
-			return;
-		event.setDamageModifier(Critical.getCritAmount(lvl, event.getDamageModifier()));
-	}
-
-	@OnlyIn(Dist.CLIENT)
-	@SubscribeEvent
-	public void onJump(InputEvent.Key event) {
-		if (!this.isEnabled()
-				|| Minecraft.getInstance().player == null
-				|| event.getAction() != InputConstants.PRESS
-				|| event.getKey() != Minecraft.getInstance().options.keyJump.getKey().getValue())
-			return;
-
-		LocalPlayer player = Minecraft.getInstance().player;
-		if (DoubleJump.extraJump(player)) {
-			JumpMidAirMessage.jumpMidAir(player);
-		}
-	}
-
-	@OnlyIn(Dist.CLIENT)
-	@SubscribeEvent
-	public void onLivingFall(LivingFallEvent event) {
-		if (!this.isEnabled())
-			return;
-
-		if (event.getEntity() instanceof LocalPlayer player)
-			player.getPersistentData().putInt("double_jumps", 0);
-		GravityDefying.applyFallDamageReduction(event);
 	}
 
 	public void baneOfSssssOnAttack(LivingEntity attacker, LivingEntity entity, LivingHurtEvent event) {
@@ -305,18 +157,6 @@ public class EnchantmentsFeature extends JsonFeature {
 			return;
 
 		event.setAmount((event.getAmount() + 2.5f * lvl));
-	}
-
-	public void waterCoolantOnAttack(LivingEntity attacker, LivingEntity entity, LivingHurtEvent event) {
-		if (!Utils.isEntityInTag(entity, WATER_COOLANT_AFFECTED))
-			return;
-
-		int lvl = attacker.getMainHandItem().getEnchantmentLevel(WATER_COOLANT.get());
-		if (lvl == 0)
-			return;
-
-		float bonusDamage = 2.5f * lvl;
-		event.setAmount((event.getAmount() + bonusDamage));
 	}
 
 	@SubscribeEvent
