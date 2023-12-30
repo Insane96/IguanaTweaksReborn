d3d5cd0240b74db25411442b262f93b23f01047a
Alberto Del Villano <delvillano.alberto@gmail.com>
2023-12-30 18:51:49 +0100

    Debuffs

--- src/main/java/insane96mcp/iguanatweaksreborn/mixin/LivingEntityMixin.java
+++ src/main/java/insane96mcp/iguanatweaksreborn/mixin/LivingEntityMixin.java
@@ -2,6 +2,7 @@ package insane96mcp.iguanatweaksreborn.mixin;
 
 import insane96mcp.iguanatweaksreborn.module.combat.RegeneratingAbsorption;
 import insane96mcp.iguanatweaksreborn.module.movement.TerrainSlowdown;
+import insane96mcp.iguanatweaksreborn.module.sleeprespawn.tiredness.Tiredness;
 import insane96mcp.insanelib.base.Feature;
 import insane96mcp.insanelib.util.MCUtils;
 import net.minecraft.tags.DamageTypeTags;
@@ -17,8 +18,10 @@ import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import javax.annotation.Nullable;
 
@@ -35,14 +38,13 @@ public abstract class LivingEntityMixin extends Entity implements Attackable, ne
         super(pEntityType, pLevel);
     }
 
-    /*
     @Inject(method = "getCurrentSwingDuration", at = @At("HEAD"), cancellable = true)
     private void onPostDamage(CallbackInfoReturnable<Integer> cir) {
         if (this.hasEffect(Tiredness.TIRED.get())) {
             //noinspection DataFlowIssue
             cir.setReturnValue(6 + (1 + this.getEffect(Tiredness.TIRED.get()).getAmplifier()));
         }
-    }*/
+    }
 
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isSprinting()Z"), method = "jumpFromGround")
     public boolean onSprintJumpCheck(LivingEntity instance) {
