36212214c4c86a656ca1868e7154f36093bbcd66
Insane96 <delvillano.alberto@gmail.com>
2024-01-05 11:51:11 +0100

    Better creative items

--- src/main/java/insane96mcp/iguanatweaksreborn/setup/client/ClientSetup.java
+++ src/main/java/insane96mcp/iguanatweaksreborn/setup/client/ClientSetup.java
@@ -5,26 +5,32 @@ import insane96mcp.iguanatweaksreborn.module.misc.beaconconduit.BeaconConduit;
 import insane96mcp.iguanatweaksreborn.module.misc.beaconconduit.ITRBeaconRenderer;
 import insane96mcp.iguanatweaksreborn.module.misc.beaconconduit.ITRBeaconScreen;
 import insane96mcp.iguanatweaksreborn.module.sleeprespawn.death.Death;
+import insane96mcp.iguanatweaksreborn.module.world.CyanFlower;
 import insane96mcp.iguanatweaksreborn.setup.ITRRegistries;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.entity.FallingBlockRenderer;
-import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
+import java.util.function.Supplier;
 
 public class ClientSetup {
     public static void onBuildCreativeModeTabContents(final BuildCreativeModeTabContentsEvent event)
     {
         if (event.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
-            event.accept(BeaconConduit.BEACON.item());
+            addAfter(event, Items.BEACON, BeaconConduit.BEACON.item());
         }
         else if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            event.accept(Death.GRAVE.item().get());
+            addAfter(event, Items.CHAIN, Death.GRAVE.item());
+        }
+        else if (event.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
+            addAfter(event, Items.POPPY, CyanFlower.FLOWER.item());
         }
         /*if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES)
         {
@@ -179,7 +185,6 @@ public class ClientSetup {
             event.accept(CoalFire.SOUL_SAND_HELLISH_COAL_ORE.item().get());
             event.accept(CoalFire.SOUL_SOIL_HELLISH_COAL_ORE.item().get());
             event.accept(FlintExpansion.FLINT_ROCK.item().get());
-            event.accept(CyanFlower.FLOWER.item().get());
         }
         else if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(Durium.SCRAP_PIECE.get());
@@ -201,13 +206,21 @@ public class ClientSetup {
         }*/
     }
 
-    /*private static void addBefore(BuildCreativeModeTabContentsEvent event, Item before, Item itemToAdd) {
+    public static void addBefore(BuildCreativeModeTabContentsEvent event, Item before, ItemLike itemToAdd) {
         event.getEntries().putBefore(new ItemStack(before), new ItemStack(itemToAdd), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
     }
 
-    private static void addAfter(BuildCreativeModeTabContentsEvent event, Item after, Item itemToAdd) {
+    public static void addAfter(BuildCreativeModeTabContentsEvent event, Item after, ItemLike itemToAdd) {
         event.getEntries().putAfter(new ItemStack(after), new ItemStack(itemToAdd), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-    }*/
+    }
+
+    public static void addBefore(BuildCreativeModeTabContentsEvent event, Item before, Supplier<? extends ItemLike> itemToAdd) {
+        addBefore(event, before, itemToAdd.get());
+    }
+
+    public static void addAfter(BuildCreativeModeTabContentsEvent event, Item after, Supplier<? extends ItemLike> itemToAdd) {
+        addAfter(event, after, itemToAdd.get());
+    }
 
     public static void init(FMLClientSetupEvent event) {
         DecimalFormatSymbols DECIMAL_FORMAT_SYMBOLS = new DecimalFormatSymbols(Minecraft.getInstance().getLocale());
