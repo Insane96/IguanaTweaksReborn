33e98bd6539e4a423dcc96d69e823e844acb182c
Insane96 <delvillano.alberto@gmail.com>
2024-06-04 08:29:11 +0200

    Rename

--- src/main/java/insane96mcp/iguanatweaksreborn/mixin/client/GuiAccessor.java
+++ src/main/java/insane96mcp/iguanatweaksreborn/mixin/client/GuiAccessor.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(Gui.class)
-public interface GuiMixin {
+public interface GuiAccessor {
     @Accessor
     int getDisplayHealth();
 }
