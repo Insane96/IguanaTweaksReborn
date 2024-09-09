32be28876f0b3fe6e1bc7f98c2fdd0ca6ced6856
Insane96 <delvillano.alberto@gmail.com>
2024-09-09 20:39:11 +0200

    Golden tools fortune, looting and harvest levels are now configurable

--- src/main/java/insane96mcp/iguanatweaksreborn/module/mining/Gold.java
+++ src/main/java/insane96mcp/iguanatweaksreborn/module/mining/Gold.java
@@ -5,17 +5,21 @@ import insane96mcp.iguanatweaksreborn.module.Modules;
 import insane96mcp.insanelib.base.Feature;
 import insane96mcp.insanelib.base.Label;
 import insane96mcp.insanelib.base.Module;
+import insane96mcp.insanelib.base.config.Config;
 import insane96mcp.insanelib.base.config.LoadFeature;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.*;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.TierSortingRegistry;
 import net.minecraftforge.event.entity.living.LootingLevelEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
 
 @Label(name = "Gold", description = "Changes Gold tools to have an innate Fortune/Looting I and changes the harvest level to be like stone tools")
 @LoadFeature(module = Modules.Ids.MINING)
@@ -23,8 +27,26 @@ public class Gold extends Feature {
 
 	public static final String LUCKY_GOLD_TOOLTIP = IguanaTweaksReborn.MOD_ID + ".innate_luck";
 
+	@Config
+	@Label(name = "Harvest Level", description = "Set the harvest level of gold tools. Vanilla is minecraft:gold (same as minecraft:wood), the there's stone, iron, diamond, netherite. Please note that an invalid resource location or harvest level here might crash the game.")
+	public static String harvestLevel = "minecraft:stone";
+	private static ResourceLocation _harvestLevel;
+
+	@Config(min = 0, max = 255)
+	@Label(name = "Looting Level", description = "Set the innate looting level of gold tools.")
+	public static Integer lootingLevel = 1;
+	@Config(min = 0, max = 255)
+	@Label(name = "Fortune Level", description = "Set the innate fortune level of gold tools.")
+	public static Integer fortuneLevel = 1;
+
 	public Gold(Module module, boolean enabledByDefault, boolean canBeDisabled) {
 		super(module, enabledByDefault, canBeDisabled);
+	}
+
+	@Override
+	public void readConfig(ModConfigEvent event) {
+		super.readConfig(event);
+		_harvestLevel = ResourceLocation.tryParse(harvestLevel);
 	}
 
 	@SubscribeEvent
@@ -36,8 +58,8 @@ public class Gold extends Feature {
 
 		ItemStack stack = livingEntity.getMainHandItem();
 		if (stack.getItem() instanceof SwordItem swordItem && swordItem.getTier() == Tiers.GOLD) {
-			if (event.getLootingLevel() < 1)
-				event.setLootingLevel(1);
+			if (event.getLootingLevel() < lootingLevel)
+				event.setLootingLevel(lootingLevel);
 		}
 	}
 
@@ -45,18 +67,19 @@ public class Gold extends Feature {
 		if(!isEnabled(Gold.class)
 				|| !(itemStack.getItem() instanceof TieredItem tieredItem)
 				|| tieredItem.getTier() != Tiers.GOLD
-				|| prev >= 1)
+				|| prev >= fortuneLevel)
 			return prev;
 
-		return 1;
+		return fortuneLevel;
 	}
 
 	public static Tier getEffectiveTier(Tier originalTier) {
 		if(!isEnabled(Gold.class)
-				|| originalTier != Tiers.GOLD)
+				|| originalTier != Tiers.GOLD
+				|| TierSortingRegistry.getName(Tiers.GOLD) == _harvestLevel)
 			return originalTier;
 
-		return Tiers.STONE;
+		return TierSortingRegistry.byName(_harvestLevel);
 	}
 
 	@OnlyIn(Dist.CLIENT)
