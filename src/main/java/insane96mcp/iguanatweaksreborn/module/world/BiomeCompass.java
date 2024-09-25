ffd55d22ea9e2ff1bc160f92716595c8f7cdc4e3
Insane96 <delvillano.alberto@gmail.com>
2024-09-25 19:15:30 +0200

    Better biome compass biome search

--- src/main/java/insane96mcp/iguanatweaksreborn/module/world/BiomeCompass.java
+++ src/main/java/insane96mcp/iguanatweaksreborn/module/world/BiomeCompass.java
@@ -7,8 +7,6 @@ import insane96mcp.insanelib.base.Label;
 import insane96mcp.insanelib.base.LoadFeature;
 import insane96mcp.insanelib.base.Module;
 import net.minecraft.core.Holder;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
@@ -17,8 +15,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraftforge.registries.RegistryObject;
-
-import java.util.Optional;
 
 @Label(name = "Biome Compass")
 @LoadFeature(module = Modules.Ids.WORLD, canBeDisabled = false)
@@ -37,12 +33,9 @@ public class BiomeCompass extends Feature {
         public void inventoryTick(ItemStack stack, Level level, Entity entity, int itemSlot, boolean isSelected) {
             if (isSelected && entity instanceof Player player) {
                 Holder<Biome> biome = level.getBiome(player.blockPosition());
-                Optional<Registry<Biome>> registry = level.registryAccess().registry(Registries.BIOME);
-                registry.ifPresent(biomes -> {
-                    String name = biomes.getKey(biome.value()).toString();
-                    name = name.replace(':', '.');
-                    player.displayClientMessage(Component.translatable("biome." + name), true);
-                });
+                String name = biome.unwrapKey().get().location().toString();
+                name = name.replace(':', '.');
+                player.displayClientMessage(Component.translatable("biome." + name), true);
             }
         }
     }
